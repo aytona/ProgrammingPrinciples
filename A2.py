d4ed07e549896c70def332f98ef6c2f313649765
Christopher Aytona <christopher.aytona@gmail.com>
2018-03-01 12:50:55 -0500

    Made the sumTuple function return the sum of any tuple at x index instead of just at 0 index, also compressed the math operations

--- A2.py
+++ A2.py
@@ -9,7 +9,7 @@ def initScreen(colour, maxAmp):
     it initializes the screen window and returns the screen object"""
     _wn = turtle.Screen()
     _wn.colormode(255)
-    _wn.setworldcoordinates(0, -maxAmp - (maxAmp/10) , 550, maxAmp)
+    _wn.setworldcoordinates(0, -maxAmp-(maxAmp/10) , 550, maxAmp)
     while True:
         try:
             _wn.bgcolor(colour)
@@ -17,7 +17,6 @@ def initScreen(colour, maxAmp):
         except:
             pass
         colour = input('Invalid screen colour!\nNew colour: ')
-    
     return _wn
 
 def createTurtle(colour):
@@ -39,17 +38,13 @@ def harmonicsInput(num):
             except ValueError:
                 pass
                 print('Values must be in decimal format(can be float)')
-            
     return _harmonics
 
-def totalAmp(harmonic):
-    """This program takes in the harmonics list,
-    and adds all the amplitudes and returns it"""
-    _allAmps = [x[0] for x in harmonic]
-    _total = 0.0
-    for i in range(len(_allAmps)):
-        _total += _allAmps[i]
-    return _total
+def sumTuple(tList, index):
+    """This function takes in the list of tuples, and the index of the tuple
+    It then returns the sum of all of this numbers in that specific tuple index"""
+    _allNums = [x[index] for x in tList]
+    return sum(_allNums)
 
 def generateColour(turtColours):
     """This function takes in the list of already used colours,
@@ -76,7 +71,6 @@ def initTurtleList(num, colourList):
     for i in range(num):
         generateColour(colourList)
         _turtleList.append(createTurtle(colourList[-1]))
-
     return _turtleList
 
 def drawSineWaves(turtles, harmonic):
@@ -89,7 +83,7 @@ def drawHarmonics(turt, harmonic):
     """This function takes in the turtle object, and the harmonic inputs
     to draw each individual harmonic"""
     for x in range(361):
-        turt.goto(x, harmonic[0] * math.sin(math.radians(x * harmonic[1])))
+        turt.goto(x, harmonic[0]*math.sin(math.radians(x*harmonic[1])))
 
 def sumHarmonic(turt, harmonic):
     """This function is similar to drawHarmonics but instead of taking in a tuple,
@@ -100,7 +94,7 @@ def sumHarmonic(turt, harmonic):
     for x in range(361):
         y = 0
         for i in range(len(harmonic)):
-            y += _amps[i] * math.sin(math.radians(x * _freqs[i]))
+            y += _amps[i]*math.sin(math.radians(x*_freqs[i]))
         turt.goto(x, y)
 
 def sineDoubleFreq(amount):
@@ -109,8 +103,7 @@ def sineDoubleFreq(amount):
     It then returns the list of all harmonic tuples"""
     _harmonics = []
     for i in range(amount):
-        _harmonics.append((100.0/(i+1.0)*2.0, (i + 1.0) * 2.0))
-
+        _harmonics.append((100.0/(i+1.0)*2.0, (i+1.0)*2.0))
     return _harmonics
 
 def assignmentEven(sColour, tColours):
@@ -118,50 +111,48 @@ def assignmentEven(sColour, tColours):
     _firstAmp = 1/3
     _secondAmp = 1/5
     _harmonics = [(1.0, 1.0), (_firstAmp, 3.0), (_secondAmp, 5.0)]
-    _totalAmp = totalAmp(_harmonics)
+    _totalAmp = sumTuple(_harmonics, 0)
     _wn = initScreen(sColour, _totalAmp)
     _turtleList = initTurtleList(len(_harmonics), tColours)
     drawSineWaves(_turtleList, _harmonics)
-    writeFormulas(_harmonics, tColours, _totalAmp)
+    writeFormulas(_harmonics, _turtleList, _totalAmp)
     return _wn
 
 def assignmentOdd(sColour, tColours):
     """This function executes part 2 of assignment 2"""    
     _waveAmount = int(input('Amount of sine waves: '))
     _harmonics = sineDoubleFreq(_waveAmount)
-    _totalAmp = totalAmp(_harmonics)
+    _totalAmp = sumTuple(_harmonics, 0)
     _wn = initScreen(sColour, _totalAmp)
     _turtleList = initTurtleList(len(_harmonics), tColours)
     drawSineWaves(_turtleList, _harmonics)
-    writeFormulas(_harmonics, tColours, _totalAmp)
+    writeFormulas(_harmonics, _turtleList, _totalAmp)
     return _wn
 
 def userHarmonics(sColour, tColours):
     """This function executes the program according to user inputs"""
     _numOfHarmonics = int(input('Number of harmonics: '))
     _harmonics = harmonicsInput(_numOfHarmonics)
-    _totalAmp = totalAmp(_harmonics)
+    _totalAmp = sumTuple(_harmonics, 0)
     _wn = initScreen(sColour, _totalAmp)
     _turtleList = initTurtleList(_numOfHarmonics, tColours)
     drawSineWaves(_turtleList, _harmonics)
-    writeFormulas(_harmonics, tColours, _totalAmp)
+    writeFormulas(_harmonics, _turtleList, _totalAmp)
     return _wn
 
-def writeFormulas(harmonics, tColours, maxAmp):
+def writeFormulas(harmonics, tList, maxAmp):
     """This function writes out the formulas of each wave"""
     _amps = [amp[0] for amp in harmonics]
     _freqs = [freq[1] for freq in harmonics]
     _amps.insert(0, sum(_amps))
     _freqs.insert(0, sum(_freqs))
-    _textBuffer = maxAmp/len(tColours)
-    for i in range(len(tColours)):
-        _turt = turtle.Turtle()
+    _textBuffer = maxAmp/len(tList)
+    for i in range(len(tList)):
+        _turt = tList[i].clone()
         _turt.hideturtle()
-        _turt.color(tColours[i])
         _turt.penup()
         _turt.goto(375, maxAmp-_textBuffer-(_textBuffer*i))
         _turt.write('y={:g}sin({:g}f)'.format(round(_amps[i],3),round(_freqs[i],3)), align='left', font=('Arial', 14, 'normal'))
-
 
 def main():
     programType = input('Welcome!\nType "even" for part 1 or "odd" for part 2.\nAny other input will let users add and define as much harmonics as they want.\nInput: ').lower()
