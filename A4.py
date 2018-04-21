bb2759b92db73f706512c52beb483b76fe462203
aytona <christopher.aytona@gmail.com>
2018-04-21 17:37:45 -0400

    Ability to distinguish duplicates

--- A4.py
+++ A4.py
@@ -49,8 +49,13 @@ class Parent(Grand_Parent):
 
 class Children(Parent):
     def Percentage(self, alien):
-        complement_self = list(set(self.chromosomes) - set(alien.Get_Chromosomes()))
-        return len([chromosome for chromosome in self.chromosomes if chromosome not in complement_self])/len(self.chromosomes)
+        clone_alien = list(alien.Get_Chromosomes())
+        union_list = []
+        for i in range(len(self.chromosomes)):
+            if self.chromosomes[i] in clone_alien:
+                union_list.append(self.chromosomes[i])
+                clone_alien.remove(self.chromosomes[i])
+        return len(union_list)/len(self.chromosomes)
 
 if __name__ == "__main__":
     GrandMother = Grand_Parent("GrandMother", False)
